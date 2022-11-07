FIX: major bug in location masking. fix by obfuscating within radius

foursquare_privacy/location_masking.py
@@ -18,9 +18,12 @@ class LocationMasker:
             Perturbation in meter, by default 20
         """
         data_masked = self.data.copy()
-        # make random unit vectors and scale by masking factor
+        # make random unit vectors
         translation = np.random.rand(len(data_masked), 2) - 0.5
-        translation = translation / np.expand_dims(np.linalg.norm(translation, axis=1), 1) * masking_factor
+        # scale by random factor --> maximally the masking factor
+        translation_size = np.expand_dims(np.random.uniform(low=0, high=masking_factor, size=len(translation)), 1)
+        # translata by unit vector multiplied by factor
+        translation = translation / np.expand_dims(np.linalg.norm(translation, axis=1), 1) * translation_size
 
         data_masked["latitude"] = data_masked["latitude"].values + translation[:, 0]
         data_masked["longitude"] = data_masked["longitude"].values + translation[:, 1]

foursquare_privacy/plotting.py
@@ -6,12 +6,17 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 
-def plot_label_distribution(labels, out_path=os.path.join("data", "label_distribution.png")):
-    uni, counts = np.unique(labels, return_counts=True)
+def poi_dist_plot(label_list, out_path=None, size=(8, 6), title="Foursquare POI distribution"):
+    uni, counts = np.unique(label_list, return_counts=True)
+    plt.figure(figsize=size)
     plt.bar(uni, counts)
     plt.xticks(rotation=90)
-    plt.savefig(out_path)
-    plt.show()
+    plt.title(title)
+    plt.tight_layout()
+    if out_path is not None:
+        plt.savefig(out_path)
+    else:
+        plt.show()
 
 
 def plot_confusion_matrix(labels, pred, col_names=None, normalize="sensitivity", out_path=None):
@@ -67,7 +72,7 @@ def plot_confusion_matrix(labels, pred, col_names=None, normalize="sensitivity",
         bottom=False,  # ticks along the bottom edge are off
         top=False,  # ticks along the top edge are off
         labelbottom=True,
-        rotation=0,
+        rotation=90,
     )
     plt.xlabel("$\\bf{Predictions}$", fontsize=20)
     plt.ylabel("$\\bf{Ground\ truth}$", fontsize=20)
@@ -78,7 +83,7 @@ def plot_confusion_matrix(labels, pred, col_names=None, normalize="sensitivity",
         plt.savefig(out_path)
 
 
-def main_plot(result_df, out_path):
+def main_plot(result_df, out_path=None):
     result_df.sort_values(["obfuscation", "method"], inplace=True)
     data_feats = result_df[result_df["method"] == "all features"]
     data_nearest = result_df[result_df["method"] == "spatial join"]
@@ -101,15 +106,18 @@ def main_plot(result_df, out_path):
         linestyle="--",
         c="grey",
     )
-    plt.xlabel("Obfuscation (in meters)")
+    plt.xlabel("Obfuscation radius (in meters)")
     plt.ylabel("Accuracy")
     # plt.xticks(np.arange(len(data_nearest)), data_nearest["obfuscation"])
     plt.legend()
     plt.tight_layout()
-    plt.savefig(os.path.join(out_path, "main_result_plot.png"))
+    if out_path is not None:
+        plt.savefig(os.path.join(out_path, "main_result_plot.png"))
+    else:
+        plt.show()
 
 
-def user_mae_plot(result_df, out_path, metric="User-wise MAE"):
+def user_mae_plot(result_df, out_path=None, metric="User-wise MAE"):
     metric_probs = metric + " probs"
     user_results = result_df.dropna(subset=[metric_probs])
     user_results = user_results.sort_values(["obfuscation", "method"])
@@ -153,7 +161,7 @@ def user_mae_plot(result_df, out_path, metric="User-wise MAE"):
                 )
             plot_lines_inner.append(l1)
         plot_lines.append(plot_lines_inner)
-    plt.xlabel("Obfuscation (in meters)")
+    plt.xlabel("Obfuscation radius (in meters)")
     label_mapping = {
         "User-wise MAE": "User-wise distribution MAE",
         "Profile identification": "User identification (top-5 accuracy)",
@@ -167,11 +175,19 @@ def user_mae_plot(result_df, out_path, metric="User-wise MAE"):
         plt.legend([l[0] for l in plot_lines], feat_labels, loc="lower right")
     plt.gca().add_artist(legend1)
     plt.tight_layout()
-    plt.savefig(os.path.join(out_path, f"user_performance_({metric}).png"))
+    if out_path is not None:
+        plt.savefig(os.path.join(out_path, f"user_performance_({metric}).png"))
+    else:
+        plt.show()
 
 
 def plot_configurations(
-    all_results, compare_col="split", compare_second_level="poi_data", eval_col="Accuracy", agg_method="mean"
+    all_results,
+    compare_col="split",
+    compare_second_level="poi_data",
+    eval_col="Accuracy",
+    agg_method="mean",
+    out_path="figures",
 ):
     cols_avail = ["method", "model", "poi_data", "city", "split"]
     filter_columns = {"split": "spatial", "model": "xgb"}  # "poi_data": "foursquare",
@@ -238,7 +254,7 @@ def plot_configurations(
         title=compare_second_level.replace("_", " "),
     )
     plt.ylabel(eval_col)
-    plt.xlabel("Location masking (in m)")
+    plt.xlabel("Obfuscation radius (in m)")
     plt.legend(
         [l[0] for l in plot_lines],
         results_filtered[compare_col].unique(),
@@ -248,8 +264,10 @@ def plot_configurations(
     plt.gca().add_artist(legend1)
     filtered_keys = "_".join(list(filter_columns.values()))
     plt.tight_layout()
-    plt.savefig(f"../figures/{compare_col}_{compare_second_level}_{eval_col}_({filtered_keys}).png")
-    plt.show()
+    if out_path is not None:
+        plt.savefig(os.path.join(out_path, f"{compare_col}_{compare_second_level}_{eval_col}_({filtered_keys}).png"))
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":

scripts/run.py
@@ -17,7 +17,7 @@ model_dict = {"xgb": {"model_class": XGBWrapper, "config": {}}, "mlp": {"model_c
 # xgb config: tree_method="gpu_hist", gpu_id=0 if gpu available
 
 
-def cross_validation(dataset, folds, models=[], save_name=None, load_name=None):
+def cross_validation(dataset, folds, models=[], save_name=None, load_name=None, save_feature_importance=False):
     if len(models) == 0 and load_name is None:
         print("Training models...")
     else:
@@ -66,6 +66,10 @@ def cross_validation(dataset, folds, models=[], save_name=None, load_name=None):
         result_df.loc[fold_samples, proba_columns] = y_pred_proba
         result_df.loc[fold_samples, "prediction"] = y_pred
         # print(f"Accuracy fold {fold_num+1}:", sum(y_pred == test_y) / len(test_y))
+    # save features importance
+    if save_feature_importance:
+        with open(os.path.join(out_dir, f"feature_importance_{masking}.json"), "w") as outfile:
+            json.dump(models[0].feature_importance, outfile)
     return models, result_df
 
 
@@ -90,6 +94,8 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--fold_mode", default="spatial", type=str)
     parser.add_argument("-k", "--kfold", default=4, type=int)
     parser.add_argument("-b", "--buffer_factor", default=1.5, type=float)
+    # minimum buffer around the location, aside from the buffer factor
+    parser.add_argument("--min_buffer", default=100, type=float)
     parser.add_argument("-l", "--lda", action="store_true")
     args = parser.parse_args()
 
@@ -110,7 +116,14 @@ if __name__ == "__main__":
 
     # load data
     data_raw = read_gdf_csv(os.path.join(args.data_path, f"checkin_{city}_features.csv"))
-    pois = read_poi_geojson(os.path.join(args.data_path, f"pois_{city}_{args.poi_data}.geojson"))
+    if args.poi_data == "both":
+        pois_1 = read_poi_geojson(os.path.join(args.data_path, f"pois_{city}_foursquare.geojson"))
+        pois_2 = read_poi_geojson(os.path.join(args.data_path, f"pois_{city}_osm.geojson"))
+        pois = pd.concat((pois_2[["id", "poi_my_label", "poi_type", "geometry"]], pois_1))
+        pois_1, pois_2 = None, None  # free space of variable
+    else:
+        pois = read_poi_geojson(os.path.join(args.data_path, f"pois_{city}_{args.poi_data}.geojson"))
+    print(f"Using {len(pois)} pois")
 
     # Split data
     np.random.seed(42)
@@ -126,15 +139,11 @@ if __name__ == "__main__":
     _, results_only_user = cross_validation(data_raw, folds)
     print_results(results_only_user, "temporal_features")
 
-    # # Uncomment to keep only the spacial features:
-    # drop_cols = [col for col in data_raw.columns if col.startswith("feat")]
-    # print("Dropping features: ", drop_cols)
-    # data_raw.drop(drop_cols, axis=1, inplace=True)
-    # print(data_raw.columns)
+    temporal_feats = [col for col in data_raw.columns if col.startswith("feat")]
 
     # obfuscate coordinates
-    for masking in [0, 25, 50, 100, 200, 400, 800, 1600]:
-        buffering = max(args.buffer_factor * masking, 400)
+    for masking in [25, 50, 100, 200, 400, 800, 1200, 1600]:
+        buffering = max(args.buffer_factor * masking, args.min_buffer)
         print(f"-------- Masking {masking} ---------")
         if masking == 0:
             data = data_raw.copy()
@@ -162,6 +171,7 @@ if __name__ == "__main__":
             lda_features = poi_process.lda_features()
             assert len(poi_features) == len(lda_features)
             poi_features = poi_features.merge(lda_features, left_index=True, right_index=True)
+        del poi_process
 
         # version 2: together with user features
         dataset = data.merge(poi_features, left_on=["latitude", "longitude"], right_index=True, how="left")
@@ -171,13 +181,18 @@ if __name__ == "__main__":
         # if any(pd.isna(dataset)):
         #     print("Attention: NaNs in data", sum(pd.isna(dataset)))
 
-        _, result_df = cross_validation(dataset, folds, models=[])
+        _, result_df = cross_validation(dataset, folds, models=[], save_feature_importance=True)
+        print_results(result_df, f"all_features_{masking}")
+
+        dataset_spatial = dataset.drop(temporal_feats, axis=1)
+        _, result_df = cross_validation(dataset_spatial, folds, models=[])
+        print_results(result_df, f"spatial_features_{masking}")
+
         # # CODE to train only on non-obfuscated and test on others
         # if masking == 0:
         #     models, result_df = cross_validation(dataset, folds, models)
         # else:
         #     _, result_df = cross_validation(dataset, folds, models)
-        print_results(result_df, f"all_features_{masking}")
 
     with open(os.path.join(out_dir, "results.json"), "w") as outfile:
         json.dump(results_dict, outfile)